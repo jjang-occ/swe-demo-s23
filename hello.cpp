ee6f4a235ec755ae467d468459826ce8fb7459cd
Oliver A. Kelton <oakelton@gmail.com>
2023-05-16 14:59:51 -0700

    created hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, const char **argv)
+{
+    using namespace std;
+
+    cout << "Hello, World!" << endl;
+
+    return EXIT_SUCCESS;
+}// end main
